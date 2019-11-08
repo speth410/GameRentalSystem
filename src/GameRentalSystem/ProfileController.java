4fcbae9a38be7f5e148421189c299d795d3005d1
speth410 <nicholass410@gmail.com>
2019-11-08 14:34:50 -0500

    Fixed color of the price displayed under each game in the game list.

--- src/GameRentalSystem/ProfileController.java
+++ src/GameRentalSystem/ProfileController.java
@@ -10,10 +10,9 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text; ///
+import javafx.scene.text.Text;
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
