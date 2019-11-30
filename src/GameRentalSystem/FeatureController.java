53527c3c1ab22c1dda4743397f5efbcf779a3439
shane <smmiller4337@eagle.fgcu.edu>
2019-11-30 17:03:37 -0500

    latest version(font error fixed)

--- src/GameRentalSystem/FeatureController.java
+++ src/GameRentalSystem/FeatureController.java
@@ -1,23 +1,16 @@
 package GameRentalSystem;
 
-import com.sun.jdi.LongValue;
-import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import javafx.animation.*;
-import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.effect.Blend;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
 import java.util.Random;
-import java.util.concurrent.Delayed;
 
 public class FeatureController {
 
@@ -31,6 +24,7 @@ public class FeatureController {
   @FXML private Blend noHoverEffect = new Blend();
   Image img1 = new Image("GameRentalSystem/images/feature1.jpg");
   Image img2 = new Image("GameRentalSystem/images/feature2.jpg");
+  Image[] img = {img1, img2};
 
   private void makeFade() {
     FadeTransition fadeTransition = new FadeTransition();
@@ -41,31 +35,11 @@ public class FeatureController {
     fadeTransition.play();
   }
 
-  public void featuredGames() {
-    Image[] img = {img1, img2};
-    Random rand = new Random();
-    int limit = rand.nextInt(2) + 1;
-
-    if (limit == 1) {
-      featureImg.setImage(img1);
-      selectImg1.setEffect(hovereffect);
-    } else {
-      selectImg1.setEffect(noHoverEffect);
-    }
-
-    if (limit == 2) {
-      featureImg.setImage(img2);
-      selectimg2.setEffect(hovereffect);
-    } else {
-      selectimg2.setEffect(noHoverEffect);
-    }
-
-    makeFade();
-  }
-
   @FXML
   public void initialize() {
-    featuredGames();
+    selectImg1.setEffect(hovereffect);
+    Random rand = new Random(2);//always start on COD(replace w/ 0 for random)
+    featureImg.setImage(img[rand.nextInt(img.length)]);
     hovereffect.setMode(BlendMode.ADD);
     hovereffect.setOpacity(0.6);
     noHoverEffect.setOpacity(0);
@@ -73,12 +47,42 @@ public class FeatureController {
 
   @FXML
   void rightClicked(MouseEvent event) {
-    featuredGames();
+    if (featureImg.getImage() != img[0]) {
+      featureImg.setImage(img[0]);
+      selectimg2.setEffect(hovereffect);
+    }
+    else if(featureImg.getImage() != img[1]) {
+      selectImg1.setEffect(hovereffect);
+      featureImg.setImage(img[1]);
+      }
+
+    if (featureImg.getImage() == img[1]) {
+      selectimg2.setEffect(noHoverEffect);
+    }
+    else if(featureImg.getImage() == img[0]) {
+      selectImg1.setEffect(noHoverEffect);
+    }
+
+      makeFade();
   }
 
   @FXML
   void leftClicked(MouseEvent event) {
-    featuredGames();
+    if (featureImg.getImage() != img[1]) {
+      featureImg.setImage(img[1]);
+      selectImg1.setEffect(hovereffect);
+    }
+    else if(featureImg.getImage() != img[0]) {
+        selectimg2.setEffect(hovereffect);
+        featureImg.setImage(img[0]);
+    }
+      if (featureImg.getImage() == img[0]) {
+          selectImg1.setEffect(noHoverEffect);
+      }
+      else if(featureImg.getImage() == img[1]) {
+          selectimg2.setEffect(noHoverEffect);
+      }
+    makeFade();
   }
 
   /** Effects */
